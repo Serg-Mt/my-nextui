32e044be924fcbbe61877fba988e61d4f0a56266
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-10-24 20:13:47 +0400

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,55 +7,71 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Button, ButtonGroup } from "@nextui-org/button";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
 
 export default function IndexPage() {
-	return (
-		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-					<br />
-					<h1 className={title()}>
-						websites regardless of your design experience.
-					</h1>
-					<h4 className={subtitle({ class: "mt-4" })}>
-						Beautiful, fast and modern React UI library.
-					</h4>
-				</div>
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <Button color="primary">
+            Button
+          </Button>
+          <Popover placement="right">
+            <PopoverTrigger>
+              <Button>Open Popover</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="px-1 py-2">
+                <div className="text-small font-bold">Popover Content</div>
+                <div className="text-tiny">This is the popover content</div>
+              </div>
+            </PopoverContent>
+          </Popover>
+          <h1 className={title()}>Make&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+          <br />
+          <h1 className={title()}>
+            websites regardless of your design experience.
+          </h1>
+          <h4 className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+          </h4>
+        </div>
 
-				<div className="flex gap-3">
-					<Link
-						isExternal
-						as={NextLink}
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
-				</div>
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            as={NextLink}
+            href={siteConfig.links.docs}
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+          >
+            Documentation
+          </Link>
+          <Link
+            isExternal
+            as={NextLink}
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
 
-				<div className="mt-8">
-					<Snippet hideSymbol hideCopyButton variant="bordered">
-						<span>
-							Get started by editing <Code color="primary">pages/index.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
-			</section>
-		</DefaultLayout>
-	);
+        <div className="mt-8">
+          <Snippet hideSymbol hideCopyButton variant="bordered">
+            <span>
+              Get started by editing <Code color="primary">pages/index.tsx</Code>
+            </span>
+          </Snippet>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
 }
